446f7ebdc2b3b84a005de772086ad49b60a5b7a6
stepholo <oloobrian89@gmail.com>
2025-07-29 16:46:25 +0300

    Add product and category urls

---
+++ shopvana/products/urls.py
@@ -0,0 +1,32 @@
+from .views import CategoryViewSet, ProductViewSet
+from django.urls import path
+
+
+url_patterns = [
+    path(
+        'categories/',
+        CategoryViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='category-list'
+    ),
+    path(
+        'categories/<int:pk>/',
+        CategoryViewSet.as_view({'get': 'retrieve',
+                                 'put': 'update',
+                                 'delete': 'destroy'}),
+        name='category-detail'
+    ),
+    path(
+        'products/',
+        ProductViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='product-list'
+    ),
+    path(
+        'products/<int:pk>/',
+        ProductViewSet.as_view({
+            'get': 'retrieve',
+            'put': 'update',
+            'delete': 'destroy'
+            }),
+        name='product-detail'
+        ),
+]
