51975e0c958a16a3f3f8f43979ec2483c2045ddd
stepholo <oloobrian89@gmail.com>
2025-07-30 14:16:17 +0300

    Correcting the review urls

--- shopvana/reviews/urls.py
+++ shopvana/reviews/urls.py
@@ -1,39 +1,35 @@
 from .views import ReviewViewSet
 from django.urls import path
 
-"""
-This file defines the URL patterns for the reviews app,
-mapping URLs to the ReviewViewSet methods.
-"""
-
 urlpatterns = [
     path(
         'reviews/',
         ReviewViewSet.as_view({'get': 'list', 'post': 'create'}),
         name='review-list'
-        ),
+    ),
     path(
         'reviews/<uuid:pk>/',
         ReviewViewSet.as_view({
-            'get': 'retrieve', 'put':
-            'update', 'patch': 'partial_update',
+            'get': 'retrieve',
+            'put': 'update',
+            'patch': 'partial_update',
             'delete': 'destroy'
-            }),
+        }),
         name='review-detail'
-        ),
+    ),
     path(
-        'reviews/product/<uuid:product_id>/',
+        'reviews/product/<uuid:product>/',
         ReviewViewSet.as_view({'get': 'list'}),
         name='review-by-product'
-        ),
+    ),
     path(
-        'reviews/user/<uuid:user_id>/',
+        'reviews/user/<uuid:user>/',
         ReviewViewSet.as_view({'get': 'list'}),
         name='review-by-user'
-        ),
+    ),
     path(
-        'reviews/<uuid:product_id>/user/<uuid:user_id>/',
+        'reviews/<uuid:product>/user/<uuid:user>/',
         ReviewViewSet.as_view({'get': 'list'}),
         name='review-by-product-and-user'
-        ),
+    ),
 ]
