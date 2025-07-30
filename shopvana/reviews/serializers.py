35e7dd6dfee787a19f4c4d7aa8b7c5b242e75b3f
stepholo <oloobrian89@gmail.com>
2025-07-30 16:12:13 +0300

    Correcting review serializers

--- shopvana/reviews/serializers.py
+++ shopvana/reviews/serializers.py
@@ -20,3 +20,13 @@ class ReviewSerializer(serializers.ModelSerializer):
                 message="You have already reviewed this product."
             )
         ]
+
+        def validate_rating(self, value: int) -> int:
+            """Validate that the rating is an int and is between 1 and 5."""
+            if not isinstance(value, int):
+                raise serializers.ValidationError("Rating must be an integer.")
+            if value < 1 or value > 5:
+                raise serializers.ValidationError(
+                    "Rating must be between 1 and 5."
+                    )
+            return value
