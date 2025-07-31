4e8e5593548996e13325905f9fe8f162f7c88fc6
stepholo <oloobrian89@gmail.com>
2025-07-31 19:38:02 +0300

    Correcting wishlist admin

--- shopvana/wishlists/admin.py
+++ shopvana/wishlists/admin.py
@@ -15,7 +15,10 @@ class WishlistAdmin(admin.ModelAdmin):
 @admin.register(WishlistItem)
 class WishlistItemAdmin(admin.ModelAdmin):
     """Admin interface for managing Wishlist Items."""
-    list_display = ('item_id', 'wishlist', 'product_id', 'user_id', 'added_on', 'updated_at')
+    list_display = (
+        'item_id', 'wishlist', 'product_id',
+        'user_id', 'added_on', 'updated_at'
+        )
     search_fields = ('wishlist__name', 'product_id__name', 'user_id__username')
     list_filter = ('added_on',)
     ordering = ('-added_on',)
