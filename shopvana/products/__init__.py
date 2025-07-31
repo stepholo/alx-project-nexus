1de19a2670a2495e722878b2ec649e7280d9e596
stepholo <oloobrian89@gmail.com>
2025-07-31 18:32:47 +0300

    Adding signal

---
+++ shopvana/products/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'shopvana.products.apps.ProductsConfig'
--- shopvana/products/apps.py
+++ shopvana/products/apps.py
@@ -4,3 +4,17 @@ from django.apps import AppConfig
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
+
+    def ready(self):
+        # Import signals to ensure they are registered
+        import products.signals
+        # Import managers to ensure they are available
+        import products.models
+        # Import admin to ensure admin configurations are loaded
+        import products.admin
+        # Import migrations to ensure they are applied
+        import products.migrations
+        # Import views to ensure they are registered
+        import products.views
+        # Import serializers to ensure they are available for API usage
+        import products.serializers
