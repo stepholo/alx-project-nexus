bf1a21dd54f8bb6615d949e85a3b51b650b769d6
stepholo <oloobrian89@gmail.com>
2025-08-12 22:26:08 +0300

    Adding background celery tasks

--- shopvana/utils/tasks.py
+++ shopvana/utils/tasks.py
@@ -2,9 +2,16 @@ from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
-from celery import shared_task
-from payments.views import PaymentViewSet
+from payments.models import Payment
+from django.conf import settings
+import requests
+import logging
 
+
+logger = logging.getLogger(__name__)
+
+CHAPA_VERIFY_URL = f"{settings.CHAPA_BASE_URL.rstrip('/')}/transaction/verify/"
+CHAPA_SECRET_KEY = settings.CHAPA_SECRET_KEY
 
 @shared_task
 def send_email_async(subject, template_name, context, recipient_list):
@@ -23,11 +30,30 @@ def send_email_async(subject, template_name, context, recipient_list):
         message,
         settings.DEFAULT_FROM_EMAIL,
         recipient_list=recipient_list,
-        html_message=html_message,
+        html_message=message,
         fail_silently=False,
     )
 
-
 @shared_task
-def simulate_payment_status_update(checkout_request_id):
-    PaymentViewSet().simulate_callback(checkout_request_id)
+def check_pending_payments():
+    pending_payments = Payment.objects.filter(status="pending")
+    headers = {"Authorization": f"Bearer {CHAPA_SECRET_KEY}"}
+
+    for payment in pending_payments:
+        tx_ref = payment.chapa_tx_ref
+        try:
+            chapa_resp = requests.get(f"{CHAPA_VERIFY_URL}{tx_ref}", headers=headers)
+            if chapa_resp.status_code == 200:
+                resp_data = chapa_resp.json()
+                status_str = resp_data['data']['status']
+                if status_str == "success":
+                    payment.status = "completed"
+                    payment.order.status = "paid"
+                    payment.order.save()
+                elif status_str == "failed":
+                    payment.status = "failed"
+                    payment.order.status = "cancelled"
+                    payment.order.save()
+                payment.save()
+        except Exception as e:
+            logger.error(f"Error checking payment {tx_ref}: {e}")
