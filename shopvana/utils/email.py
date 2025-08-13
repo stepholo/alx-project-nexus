3d6e06b73bda1a0d0146b5561adcb1ba1985200d
stepholo <oloobrian89@gmail.com>
2025-08-13 18:48:58 +0300

    Updating synch tasks

--- shopvana/utils/email.py
+++ shopvana/utils/email.py
@@ -2,7 +2,7 @@ from utils.tasks import send_email_async
 import logging
 
 
-def send_notification_email(to_email, subject, template_name, context):
+def send_notification_email(to_email: str, subject: str, template_name: str, context: dict) -> None:
     # Offload the email sending task to Celery
     logging.info(f"Sending email to {to_email} with subject: {subject}")
     send_email_async.delay(subject, template_name, context, [to_email])
