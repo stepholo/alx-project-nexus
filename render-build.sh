cc75c6fc42c4010863ba4e6f382aff223ace2090
stepholo <oloobrian89@gmail.com>
2025-08-15 13:17:33 +0300

    Adding whitenoise for render deployment

--- render-build.sh
+++ render-build.sh
@@ -10,15 +10,14 @@ pip install -r requirements.txt
 # 2. Apply migrations
 python shopvana/manage.py migrate
 
+# Collect static files
+python shopvana/manage.py collectstatic --noinput
+
 # 4. Create a superuser if not exists
 python shopvana/manage.py createsuperuser --noinput || true
-
-find . -name "settings.py"
 
 pwd; ls -l
 
 cd shopvana
 
 pwd; ls -l
-
-find . -name "settings.py"
--- shopvana/shopvana/settings.py
+++ shopvana/shopvana/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'shopvana.urls'
@@ -149,6 +150,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Let whitenoise serve compressed files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
