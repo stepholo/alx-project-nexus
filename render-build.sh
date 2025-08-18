2ecde0e5d7abd7a0423ac039b282debb1f59b48a
stepholo <oloobrian89@gmail.com>
2025-08-18 22:32:08 +0300

    render build

--- render-build.sh
+++ render-build.sh
@@ -18,13 +18,19 @@ User = get_user_model();
 username=os.environ['DJANGO_SUPERUSER_USERNAME'];
 email=os.environ['DJANGO_SUPERUSER_EMAIL'];
 password=os.environ['DJANGO_SUPERUSER_PASSWORD'];
-user, created = User.objects.get_or_create(username=username, defaults={'email': email});
-user.is_active = True;
-user.is_staff = True;
-user.is_superuser = True;
-user.set_password(password);
-user.save();
-msg = f'>>> Superuser {username} active={user.is_active} staff={user.is_staff} superuser={user.is_superuser}';
-print(msg);
+try:
+    user = User.objects.get(username=username)
+    user.is_active = True
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
+    msg = f'>>> Existing superuser {username} updated and activated.'
+except User.DoesNotExist:
+    user = User.objects.create_superuser(username=username, email=email, password=password)
+    user.is_active = True
+    user.save()
+    msg = f'>>> New superuser {username} created and activated.'
+print(msg)
 sys.stdout.flush();
 "
