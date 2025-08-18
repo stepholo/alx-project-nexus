3b7e188f9f1f955b63dd43718c4470c707368682
stepholo <oloobrian89@gmail.com>
2025-08-18 22:16:55 +0300

    Adding render build commands

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # render-build.sh
 
-set -o errexit  # Exit on error
-
-# 1. Upgrade pip & install dependencies
-pip install --upgrade pip
+# Install dependencies
 pip install -r requirements.txt
 
-# 2. Collect static files (build-time)
+# Run migrations
+python shopvana/manage.py migrate
+
+# Collect static files
 python shopvana/manage.py collectstatic --noinput
+
+# Create superuser
+python shopvana/manage.py shell -c "
+from django.contrib.auth import get_user_model;
+import os, sys;
+User = get_user_model();
+username=os.environ['DJANGO_SUPERUSER_USERNAME'];
+email=os.environ['DJANGO_SUPERUSER_EMAIL'];
+password=os.environ['DJANGO_SUPERUSER_PASSWORD'];
+user, created = User.objects.get_or_create(username=username, defaults={'email': email});
+user.is_active = True;
+user.is_staff = True;
+user.is_superuser = True;
+user.set_password(password);
+user.save();
+msg = f'>>> Superuser {username} active={user.is_active} staff={user.is_staff} superuser={user.is_superuser}';
+print(msg);
+sys.stdout.flush();
+"
